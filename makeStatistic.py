a1371f28c0c4bc2a6f593d3e30d476a035f46d6c
Czupryniak, Michal <michal.czupryniak@pearson.com>
2018-01-14 19:53:38 +0100

    Final version

--- makeStatistic.py
+++ makeStatistic.py
@@ -1,10 +1,20 @@
+from pprint import pprint
+
 from pymongo import MongoClient
 from operator import itemgetter
 import matplotlib.pyplot as plt
 import numpy as np
 
+client = MongoClient()
+db = client['INS']
+authors_collection = db['authors']
 
-def draw_plot(collection, title, file_name, count):
+authors_in_institute = []
+for document in authors_collection.find():
+    authors_in_institute.append(document['name'])
+
+
+def draw_plot(collection, title, file_name, count, pair=False):
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
@@ -15,7 +25,10 @@ def draw_plot(collection, title, file_name, count):
     index = 0
     for authors, x in collection:
         ys.append(index)
-        authors_labels.append(authors)
+        if pair:
+            authors_labels.append(authors[0] + ' / ' + authors[1])
+        else:
+            authors_labels.append(authors)
         xs.append(x)
         index += 1
 
@@ -27,16 +40,7 @@ def draw_plot(collection, title, file_name, count):
 
 
 def collaborators_by_decade(decade):
-    client = MongoClient()
-    db = client['INS']
-    authors_collection = db['authors']
-
-    exceptions = ['...']
-    authors_in_institute = []
     authors_collaboration = dict()
-
-    for document in authors_collection.find():
-        authors_in_institute.append(document['name'])
 
     for document in authors_collection.find():
         cooperating_authors = []
@@ -47,13 +51,6 @@ def collaborators_by_decade(decade):
                         cooperating_authors.append(author.lstrip())
 
         cooperating_authors = np.unique(cooperating_authors).tolist()
-
-        for exception in exceptions:
-            if exception in cooperating_authors:
-                cooperating_authors.remove(exception)
-
-        if document['name'] in cooperating_authors:
-            cooperating_authors.remove(document['name'])
 
         cooperating_internal_authors = []
         cooperating_external_authors = []
@@ -69,8 +66,39 @@ def collaborators_by_decade(decade):
     return authors_collaboration
 
 
-def most_collaborators(decade, count, dir):
+def pairs_by_decade(decade):
+    authors_pairs_collaboration = dict()
+
+    for document in authors_collection.find():
+        cooperating_authors = []
+        for article in document['articles']:
+            if article['year'] != '':
+                if int(article['year']) in range(decade+1, decade + 10):
+                    for author in article['authors']:
+                        cooperating_authors.append(author.lstrip())
+
+        unique, counts = np.unique(cooperating_authors, return_counts=True)
+        cooperating_authors = dict(zip(unique, counts))
+
+        for key, val in cooperating_authors.items():
+            if key in authors_in_institute:
+                authors_pairs_collaboration[(document['name'], key)] = val
+
+    keys_list = set(tuple(sorted(t)) for t in authors_pairs_collaboration)
+
+    final_dict = dict()
+    for (key1, key2) in keys_list:
+        if (key1, key2) in authors_pairs_collaboration.keys():
+            final_dict[(key1, key2)] = authors_pairs_collaboration[(key1, key2)]
+        else:
+            final_dict[(key1, key2)] = authors_pairs_collaboration[(key2, key1)]
+
+    return final_dict
+
+
+def most_collaborators(decade, count, dir_to):
     authors_internal_dict = dict()
+    authors_internal_pairs_dict = pairs_by_decade(decade)
     authors_external_dict = dict()
     for key, value in collaborators_by_decade(decade).items():
         authors_internal_dict[key] = value['internal']
@@ -78,11 +106,14 @@ def most_collaborators(decade, count, dir):
 
     authors_internal_dict = sorted(authors_internal_dict.items(), key=itemgetter(1))
     authors_external_dict = sorted(authors_external_dict.items(), key=itemgetter(1))
+    authors_internal_pairs_dict = sorted(authors_internal_pairs_dict.items(), key=itemgetter(1))
 
     draw_plot(authors_internal_dict, 'Internal collaborators in ' + str(decade),
-              dir + 'internal_collaborators_' + str(decade) + '.png', count)
+              dir_to + 'internal_collaborators_' + str(decade) + '.png', count)
     draw_plot(authors_external_dict, 'External collaborators in ' + str(decade),
-              dir + 'external_collaborators_' + str(decade) + '.png', count)
+              dir_to + 'external_collaborators_' + str(decade) + '.png', count)
+    draw_plot(authors_internal_pairs_dict, 'Internal collaborators pairs in ' + str(decade),
+              dir_to + 'internal_pairs_collaborators_' + str(decade) + '.png', count, True)
 
 
 most_collaborators(2000, 10, './')
--- prepareData.py
+++ prepareData.py
@@ -1,11 +1,12 @@
 from pymongo import MongoClient
 import numpy as np
+import bson
 
 client = MongoClient()
 db = client['INS']
 authors_collection = db['authors']
 
-exceptions = ['...']
+exceptions = ['...', 'J ...']
 authors_in_institute = []
 
 for document in authors_collection.find():
@@ -15,25 +16,23 @@ for document in authors_collection.find():
     cooperating_authors = []
     for article in document['articles']:
         for author in article['authors']:
-            cooperating_authors.append(author.lstrip())
+            if author not in exceptions:
+                cooperating_authors.append(author.lstrip())
 
-    cooperating_authors = np.unique(cooperating_authors).tolist()
+    unique, counts = np.unique(cooperating_authors, return_counts=True)
+    cooperating_authors = dict(zip(unique, counts))
 
-    for exception in exceptions:
-        if exception in cooperating_authors:
-            cooperating_authors.remove(exception)
-
-    if document['name'] in cooperating_authors:
-        cooperating_authors.remove(document['name'])
-
-    cooperating_internal_authors = []
-    cooperating_external_authors = []
-    for author in cooperating_authors:
-        if author in authors_in_institute:
-            cooperating_internal_authors.append(author)
+    cooperating_internal_authors = dict()
+    cooperating_external_authors = dict()
+    for key, value in cooperating_authors.items():
+        if key in authors_in_institute:
+            cooperating_internal_authors[key] = value
         else:
-            cooperating_external_authors.append(author)
+            cooperating_external_authors[key] = value
 
-    document.update({'internal_collaborators': cooperating_internal_authors})
-    document.update({'external_collaborators': cooperating_external_authors})
+    internal_collaborators = {k: bson.Int64(v) for k, v in cooperating_internal_authors.items()}
+    external_collaborators = {k: bson.Int64(v) for k, v in cooperating_external_authors.items()}
+
+    document.update({'internal_collaborators': internal_collaborators})
+    document.update({'external_collaborators': external_collaborators})
     authors_collection.save(document)
